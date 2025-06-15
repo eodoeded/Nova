c53b81d9715fb0517b4672535ac69b85594bb56c
Max McIsted <mcistedmax@gmail.com>
2025-06-15 12:52:06 +0100

    Add lock icon to project titles

--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -1,5 +1,6 @@
 import { formatter } from "@/lib/formatter";
 import { getPosts } from "@/lib/mdx";
+import { LockIcon } from "@/components/lock-icon";
 
 import { Link as NextViewTransition } from "next-view-transitions";
 import React from "react";
@@ -30,7 +31,10 @@ export const Posts = ({ category }: PostProps) => {
       {posts.map((post) => {
         const content = (
           <div className="flex w-full justify-between py-2">
-            <p className="hover:opacity-50 transition-opacity cursor-pointer">{post.title}</p>
+            <div className="flex items-center">
+              {category === "projects" && <LockIcon />}
+              <p className="hover:opacity-50 transition-opacity cursor-pointer">{post.title}</p>
+            </div>
             <p className="mt-0 text-muted">{post.time.role}</p>
           </div>
         );
