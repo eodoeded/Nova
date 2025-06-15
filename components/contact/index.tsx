bc9f2dc81039decc46ae536da323adeb1b506ff7
Max McIsted <mcistedmax@gmail.com>
2025-06-15 12:44:32 +0100

    Update contact section layout and email

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -6,12 +6,12 @@ export const Contact = () => {
     <div className="mt-6 flex flex-col">
       <h2 className="py-2 text-muted capitalize">Contact me</h2>
       <div className="border-border border-t" />
-      <div className="flex w-full justify-between py-2">
+      <div className="flex flex-col gap-2 py-2">
         <Link href="https://www.linkedin.com/in/maxmcisted" className="hover:opacity-50 transition-opacity">
           LinkedIn
         </Link>
-        <Link href="mailto:maxmcisted@gmail.com" className="hover:opacity-50 transition-opacity">
-          maxmcisted@gmail.com
+        <Link href="mailto:mcistedm@gmail.com" className="hover:opacity-50 transition-opacity">
+          Email
         </Link>
       </div>
     </div>
