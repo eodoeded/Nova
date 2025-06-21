55cead905c054d0cc8c889fb59b498a0365fc1dd
Max McIsted <mcistedmax@gmail.com>
2025-06-21 21:56:01 +0100

    Fix: update links component and hover animations

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,35 +1,27 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
 
 export const Contact = () => {
   return (
     <div className="mt-6">
-      <h2 className="text-muted mb-2 text-sm capitalize">Links</h2>
+      <h2 className="text-muted mb-2 capitalize">Links</h2>
       <div className="border-border flex flex-col border-t">
         <Link
           href="mailto:contact@mcisted.com"
-          className="border-border flex cursor-pointer justify-between border-b py-2 transition-opacity hover:opacity-50"
+          className="border-border flex cursor-pointer justify-between border-b py-2"
         >
-          <p>Email</p>
-        </Link>
-        <Link
-          href="https://github.com/eodoeded"
-          className="border-border flex cursor-pointer justify-between border-b py-2 transition-opacity hover:opacity-50"
-          target="_blank"
-        >
-          <p>GitHub</p>
+          <p className="transition-opacity hover:opacity-50">Email</p>
         </Link>
         <div className="border-border flex justify-between border-b py-2 text-neutral-500">
           <p>CV</p>
         </div>
         <Link
           href="https://linkedin.com/in/maxmcisted"
-          className="border-border flex cursor-pointer justify-between border-b py-2 transition-opacity hover:opacity-50"
+          className="border-border flex cursor-pointer justify-between border-b py-2"
           target="_blank"
         >
-          <p>LinkedIn</p>
+          <p className="transition-opacity hover:opacity-50">LinkedIn</p>
         </Link>
       </div>
     </div>
